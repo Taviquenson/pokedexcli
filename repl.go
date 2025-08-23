40c6b29857ecaebc51f9ebb98507c057647e5405
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-08-23 16:38:04 -0700

    add repl

--- repl.go
+++ repl.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() // advances the Scanner to the next line
+
+		userWords := cleanInput(scanner.Text())
+		if len(userWords) == 0 { // Don't print message if scanned line was empty
+			continue
+		}
+
+		command := userWords[0]
+
+		fmt.Printf("Your command was: %v\n", command)
+	}
+}
 
 func cleanInput(text string) []string {
 	subStrs := strings.Fields(text)
