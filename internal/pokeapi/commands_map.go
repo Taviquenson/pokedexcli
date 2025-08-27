f45192ca858da7c653a0003a8c51239b02861241
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-08-26 18:06:08 -0700

    refactor cache

--- internal/pokeapi/commands_map.go
+++ internal/pokeapi/commands_map.go
@@ -16,30 +16,20 @@ var pokeCache = pokecache.NewCache(5 * time.Second)
 func Maps(config *Config) error {
 	bodyEntry, exists := pokeCache.Get(config.Next)
 	if exists {
-		fmt.Println("Not saved to cache")
 		listLocations(bodyEntry, config)
+		fmt.Println("Was in cache")
 		return nil
 	} else { // add to cache
-		res, err := mapRequest(config, false)
+		body, err := getBody(config, false)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		body, err := io.ReadAll(res.Body)
-		res.Body.Close()
-		if res.StatusCode > 299 {
-			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Saved to cache")
 		pokeCache.Add(config.Next, body)
 		listLocations(body, config)
+		fmt.Println("Wasn't in cache")
 		return nil
 	}
-
 }
 
 func MapsB(config *Config) error {
@@ -49,33 +39,47 @@ func MapsB(config *Config) error {
 	}
 	bodyEntry, exists := pokeCache.Get(*config.Previous)
 	if exists {
-		fmt.Println("Not saved to cache")
 		listLocations(bodyEntry, config)
+		fmt.Println("Was in cache")
 		return nil
 	} else { // add to cache
-		res, err := mapRequest(config, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if res == nil {
-			fmt.Println("you're on the first page")
-			return nil
-		}
-
-		body, err := io.ReadAll(res.Body)
-		res.Body.Close()
-		if res.StatusCode > 299 {
-			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-		}
+		body, err := getBody(config, true)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		pokeCache.Add(*config.Previous, body)
-		fmt.Println("Saved to cache")
 		listLocations(body, config)
+		fmt.Println("Wasn't in cache")
 		return nil
 	}
+}
+
+func getBody(config *Config, isMapb bool) ([]byte, error) {
+	var res *http.Response
+	var err error
+	if isMapb {
+		res, err = mapRequest(config, true)
+	} else {
+		res, err = mapRequest(config, false)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res == nil {
+		fmt.Println("you're on the first page")
+		return []byte(""), nil
+	}
+
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body, nil
 }
 
 // Makes the adequate request whther advancing or bactracking in the maps
@@ -113,6 +117,5 @@ func listLocations(body []byte, config *Config) error {
 	for _, result := range locationAreas.Results {
 		fmt.Println(result.Name)
 	}
-
 	return nil
 }
